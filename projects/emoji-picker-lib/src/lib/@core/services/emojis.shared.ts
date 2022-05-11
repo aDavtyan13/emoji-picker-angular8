9a08b6a8ecb3276e742cc0a51ae6f6982f1a2013
alla_13 <alla.davtyan99@gmail.com>
2022-05-11 18:12:09 +0400

    some checkings

--- projects/emoji-picker-lib/src/lib/@core/services/emojis.shared.ts
+++ projects/emoji-picker-lib/src/lib/@core/services/emojis.shared.ts
@@ -6,22 +6,31 @@ import {EmojiPickerService} from '../../emoji-picker-lib.service';
 
 @Injectable()
 export class EmojisSharedService {
+  public ctx: any;
   public emojis = new BehaviorSubject<IDataInterface>({} as IDataInterface);
   public data = this.emojis.asObservable();
 
   constructor(private emojiPickerService: EmojiPickerService) {
+    this.ctx = document.createElement('canvas').getContext('2d');
+    this.ctx.canvas.width = this.ctx.canvas.height = 1;
+
     this.emojiPickerService.getEmojis().then((data: IDataInterface) => {
       data.emojis.forEach((element) => {
-        element.isSupported = EmojisSharedService.supportsEmoji(element.emoji);
+        element.isSupported = this.supportsEmoji(element.emoji);
       });
       this.emojis.next(data);
     });
   }
 
-  static supportsEmoji(item: string): boolean {
-    const ctx = document.createElement('canvas').getContext('2d');
-    ctx.canvas.width = ctx.canvas.height = 1;
-    ctx.fillText(item, -4, 4);
-    return ctx.getImageData(0, 0, 1, 1).data[1] > 0;
+  private supportsEmoji(item: string): boolean {
+    this.ctx.clearRect(0, 0, 4, 4);
+    this.ctx.fillText(item, -4, 4);
+    return this.ctx.getImageData(0, 0, 1, 1).data[1] > 0;
+
+
+    // const ctx = document.createElement('canvas').getContext('2d');
+    // ctx.canvas.width = ctx.canvas.height = 1;
+    // ctx.fillText(item, -4, 4);
+    // return ctx.getImageData(0, 0, 1, 1).data[3] > 0;
   }
 }
